1fd60ceadda717c4af2bd48174949dbfd7be7fc1
gogoRcq <1551498954@qq.com>
2023-08-04 16:24:45 +0800

    1

--- src/layer/details/linear.h
+++ src/layer/details/linear.h
@@ -1,9 +1,28 @@
 #ifndef LINEAR_H_
 #define LINEAR_H_
 
+#include "layer/abstract/rcParamLayer.h"
+#include "runtime/RuntimeOperator.h"
+#include "runtime/StateCode.h"
+#include <_types/_uint32_t.h>
+#include <sys/_types/_int32_t.h>
+#include <sys/types.h>
+
 namespace rq {
 
+template<class T>
+class rcLinearLayer : public rcParamLayer<T> {
+public:
+    rcLinearLayer(int32_t inFeatures, int32_t outFeatures, bool useBias) : 
+                  inFeatures(inFeatures), outFeatures(outFeatures), useBias(useBias) {
+        if (useBias) this->initBias(1, outFeatures, 1, 1);
+    }
 
+private:
+    int32_t inFeatures = 0;
+    int32_t outFeatures = 0;
+    bool useBias = false;
+};
 
 }
 
